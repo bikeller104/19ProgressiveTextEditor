c77f05d3fbf5f1e55d9a202eb72391099054201e
Brent Keller <bikeller104@gmail.com>
2023-02-11 11:12:30 -0700

    added Inject manifest pulgin

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,6 +23,10 @@ module.exports = () => {
         template: './index.html',
         title: 'Webpack Plugin',
       }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest:'./src-sw.js' 
+      })
     ],
 
     module: {
