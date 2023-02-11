02ad20f82ac851356c8770e335d96e4e11a99be6
Brent Keller <bikeller104@gmail.com>
2023-02-11 16:32:49 -0700

    starting to add database code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log("put Request Recieved");
+
+    const dbname = 'jate'
+    const database = await openDB(dbname,1);
+    const transact = database.transaction(dbname, "readonly");
+    const store = transact.objectStore(dbname);
+    const request = store.put({id: 'content', content: content })
+    const result = await request;
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  console.log("get Request Recieved");
+    const dbname = 'jate'
+    const database = await openDB(dbname,1);
+    const transact = database.transaction(dbname, "readwrite");
+    const store = transact.objectStore(dbname);
+    const request = store.get('content')
+    const result = await request;
+} 
 
 initdb();
