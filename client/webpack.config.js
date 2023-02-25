1f1ab7bf9cbc6bff6567fb96fe14518f83ddec05
Brent Keller <bikeller104@gmail.com>
2023-02-25 13:59:10 -0700

    added manifest configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -26,6 +26,11 @@ module.exports = () => {
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest:'./src-sw.js' 
+      }),
+      new WebpackPwaManifest({
+        name: 'manifest.json',
+        fingerprint:false,
+        inject:true,
       })
     ],
 
