d070711b035b9fa5b0ef8d561bc385e0487808c3
Brent Keller <bikeller104@gmail.com>
2023-02-26 17:45:34 -0700

    more dubugging

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -5,6 +5,7 @@ const dbname = 'jate'
 const initdb = async () =>
   openDB(dbname, 1, {
     upgrade(db) {
+      console.log("upgrading database");
       if (db.objectStoreNames.contains(dbname)) {
         console.log('jate database already exists');
         return;
@@ -16,37 +17,39 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log("put Request Recieved");
+  console.log(`put Request Recieved to store ${content}`);
 
     console.log(`attempting to open database ${dbname}`);
     const database = await openDB(dbname,1);
     console.log(`opened db ${JSON.stringify(database)}`);
     const transact = database.transaction(dbname, "readwrite");
+    console.log(`created transaction ${JSON.stringify(transact)}`);
     const store = transact.objectStore(dbname);
-    const request = store.put({id: 'content', content: content })
+    console.log(`created store ${JSON.stringify(store)}`);
+   // const request = store.put({id: 'content', content: content })
+    //const request = store.put({id: 'content', content: content })
+    const request = store.put({id: 1, content: content })
+    console.log(`created request ${JSON.stringify(request)}`);
     const result = await request;
+    return result;
 }
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () =>{
-  try {
     console.log("get Request Recieved");
-    const dbname = 'jate'
+    //const dbname = 'jate'
     const database = await openDB(dbname,1);
     console.log(`opened db ${JSON.stringify(database)}`);
     const transact = database.transaction(dbname, "readonly");
     const store = transact.objectStore(dbname);
-    const request = store.get('content')
+    //const request = store.get('content')
+    const request = store.get(1);
     const result = await request;
     console.log(`queryed database for result: ${result}`)
-  }
-  catch (err) {
-      console.log(err);
-    }
-    finally {
-      result = undefined;
-    }
+
+    console.log(`returning result: ${result}`);
     return result;
 } 
 
+console.log("program started");
 initdb();
